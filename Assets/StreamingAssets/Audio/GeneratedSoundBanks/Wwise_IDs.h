7932f42b242edfad3add9537d0c711ca92690993
Fgalvane <galvane.francis@gmail.com>
2023-01-11 20:04:29 +0100

    Test Event
    Wwise FT - colors / Unity test Event

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,84 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_SFX_MS02_WP_PROJECTILE = 3075902904U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace ATTACKCOMBO
+        {
+            static const AkUniqueID GROUP = 1347076325U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID COMBO01 = 3557003812U;
+                static const AkUniqueID COMBO02 = 3557003815U;
+                static const AkUniqueID COMBO03 = 3557003814U;
+                static const AkUniqueID COMBO04 = 3557003809U;
+            } // namespace SWITCH
+        } // namespace ATTACKCOMBO
+
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID SWITCH_DIRT = 3262067523U;
+                static const AkUniqueID SWITCH_GRASS = 2902377486U;
+                static const AkUniqueID SWITCH_METAL = 1981379785U;
+                static const AkUniqueID SWITCH_MUD = 3817369922U;
+                static const AkUniqueID SWITCH_STONE = 2541504615U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID ATTACKCOMBO = 1347076325U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID TEST = 3157003241U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIANCE = 2981377429U;
+        static const AkUniqueID ENNEMIS = 1507798120U;
+        static const AkUniqueID MAINCHARACTER = 1518239843U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERBZONE_BIG = 3193884120U;
+        static const AkUniqueID REVERBZONE_CAVERN = 1438224957U;
+        static const AkUniqueID REVERBZONE_SMALL = 2399250375U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
